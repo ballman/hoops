0e0d21cd41c0b84b24662839e5478a75be18a99e
ballman <aballman@gmail.com>
2010-02-02 16:05:29 -0500

    Parse_game.rb is not used.

--- vendor/parse_game.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/ruby
-require File.dirname(__FILE__) + "/../config/environment"
-require 'rubygems'
-gem 'activerecord'
-require 'team'
-
-Team.find(:all).each { | team |
-  puts team.name
-}
