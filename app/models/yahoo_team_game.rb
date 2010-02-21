8c32062814a6fb78931f22200c997296294581ba
ballman <aballman@gmail.com>
2010-02-21 15:36:48 -0500

    Forgot this file in the prior commit.

--- app/models/yahoo_team_game.rb
+++ app/models/yahoo_team_game.rb
@@ -4,6 +4,10 @@ class YahooTeamGame < TeamGame
     player.yahoo_bs_name
   end
   
+  def add_player_game!(player, name)
+    player_games << YahooPlayerGame.new(:player => player, :player_name => name)
+  end
+
   def validate_player_total_for_stat(stat)
     player_total = player_games.inject(0) { | total, player | total + player.send(stat) }
     team_total = self.send(stat)
