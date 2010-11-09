f2715e12fef223128236d965788df0a1e69eb281
Allen <ballman@lastdog.lasts.net>
2010-11-08 22:10:21 -0500

    Added files with Rails 3.0.1 migration.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hoops::Application.config.secret_token = '3c926c32f156241687ee83d34ddd9ea242f6d150b8a8d95870fb488f97a01b7e0c6c94fabce4529a2b21c6b99efed89ebd76d6414cda41f6f9522f9cb2de4b19'
