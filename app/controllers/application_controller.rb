de4ffb266641e93020394347a8399db0d62df9c7
Allen <ballman@lastdog.lasts.net>
2010-11-08 22:05:29 -0500

    Changes made in initial step of coverting to a Rails 3.0.1 app.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,3 @@
-# The filters added to this controller will be run for all controllers in the application.
-# Likewise will all the methods added be available for all controllers.
 class ApplicationController < ActionController::Base
-  helper :calendar
-
-  before_filter :authorize, :only => [ :foo ]
-
-  def authorize
-    redirect_to :back unless session[:authorized]
-  end
-
-  def login
-    session[:authorized] = ('tricycle' == params[:key]) if params[:key]
-    redirect_to :controller => 'team', :action => 'list'
-  end
-
-  # WTF???  ---  try, try again --- new subclipse????
+  protect_from_forgery
 end
